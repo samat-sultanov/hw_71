72561b18ffc33f93c460b7f1b5b8435377f2c3f2
samat_sultanov <samatsayak@gmail.com>
2023-02-09 22:27:36 +0600

    создал crud для товаров

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from api_v1.views import ProductViewSet
 from rest_framework.routers import DefaultRouter
 
 app_name = 'api_v1'
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from webapp.models import Product
+from api_v1.serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
